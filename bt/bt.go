ff52e2ea0b2caf1fe7ac18b632404ece364bf95b
Bob Glickstein <bobg@emphatic.com>
2020-07-28 09:25:53 -0700

    Add new Google Cloud Bigtable store type (untested). Remove Google Cloud Storage store type.

---
+++ bt/bt.go
@@ -0,0 +1,288 @@
+// Package bt implements a blob store on Google BigTable.
+package bt
+
+import (
+	"context"
+	"fmt"
+	"regexp"
+	"strconv"
+	"time"
+
+	"cloud.google.com/go/bigtable"
+
+	"github.com/pkg/errors"
+
+	"github.com/bobg/bs"
+)
+
+type Store struct {
+	t *bigtable.Table
+}
+
+const (
+	anchorcol = "anchor"
+	anchorfam = "anchor"
+	blobcol   = "blob"
+	blobfam   = "blob"
+)
+
+var errEmptyItems = errors.New("empty items")
+
+func (s *Store) Get(ctx context.Context, ref bs.Ref) (bs.Blob, error) {
+	row, err := s.t.ReadRow(ctx, s.blobKey(ref))
+	if err != nil {
+		return nil, errors.Wrapf(err, "reading row %s", ref)
+	}
+	items := row[blobfam]
+	if len(items) == 0 {
+		return nil, errEmptyItems
+	}
+	return bs.Blob(items[0].Value), nil
+}
+
+func (s *Store) GetMulti(ctx context.Context, refs []bs.Ref) (bs.GetMultiResult, error) {
+	rowKeys := make(bigtable.RowList, len(refs))
+	for i, ref := range refs {
+		rowKeys[i] = s.blobKey(ref)
+	}
+
+	result := make(bs.GetMultiResult)
+
+	err := s.t.ReadRows(ctx, rowKeys, func(row bigtable.Row) bool {
+		key := row.Key()
+		ref, err := s.refFromKey(key)
+		if err != nil {
+			result[ref] = func(context.Context) (bs.Blob, error) { return nil, err }
+			return true
+		}
+		result[ref] = func(context.Context) (bs.Blob, error) {
+			items := row[blobfam]
+			if len(items) == 0 {
+				return nil, errEmptyItems
+			}
+			return bs.Blob(items[0].Value), nil
+		}
+		return true
+	})
+
+	return result, err
+}
+
+func (s *Store) GetAnchor(ctx context.Context, a bs.Anchor, when time.Time) (bs.Ref, error) {
+	var (
+		found    *bs.Ref
+		innerErr error
+	)
+
+	// Anchors come back in reverse chronological order
+	// (since we usually want the latest one).
+	// Find the first one whose timestamp is `when` or earlier.
+	err := s.eachAnchorRow(ctx, a, func(row bigtable.Row) bool {
+		key := row.Key()
+		_, atime, err := s.anchorTimeFromKey(key)
+		if err != nil {
+			innerErr = errors.Wrapf(err, "parsing anchor/time from key %s", key)
+			return false
+		}
+		if atime.After(when) {
+			return true
+		}
+		items := row[anchorfam]
+		if len(items) == 0 {
+			innerErr = errEmptyItems
+			return false
+		}
+		ref := bs.RefFromBytes(items[0].Value)
+		found = &ref
+		return false
+	})
+	if err != nil {
+		return bs.Ref{}, errors.Wrap(err, "iterating over anchor rows")
+	}
+	if innerErr != nil {
+		return bs.Ref{}, errors.Wrap(innerErr, "processing anchor row")
+	}
+	if found == nil {
+		return bs.Ref{}, bs.ErrNotFound
+	}
+	return *found, nil
+}
+
+func (s *Store) ListRefs(ctx context.Context, start bs.Ref) (<-chan bs.Ref, func() error, error) {
+	var (
+		err error
+		ch  = make(chan bs.Ref)
+	)
+
+	go func() {
+		defer close(ch)
+
+		rowFn := func(row bigtable.Row) bool {
+			key := row.Key()
+			var ref bs.Ref
+			ref, err = s.refFromKey(key)
+			if err != nil {
+				err = errors.Wrapf(err, "extracting ref from key %s", key)
+				return false
+			}
+			select {
+			case <-ctx.Done():
+				err = ctx.Err()
+				return false
+			case ch <- ref:
+				return true
+			}
+		}
+		startKey := s.blobKey(start) + "0"
+		filter := bigtable.RowKeyFilter("^b:") // blobs only
+		err = s.t.ReadRows(ctx, bigtable.InfiniteRange(startKey), rowFn, bigtable.RowFilter(filter))
+	}()
+
+	return ch, func() error { return err }, nil
+}
+
+func (s *Store) ListAnchors(ctx context.Context, start bs.Anchor) (<-chan bs.Anchor, func() error, error) {
+	var (
+		err error
+		ch  = make(chan bs.Anchor)
+	)
+
+	go func() {
+		defer close(ch)
+
+		var lastAnchor bs.Anchor
+
+		rowFn := func(row bigtable.Row) bool {
+			key := row.Key()
+			var a bs.Anchor
+			a, _, err = s.anchorTimeFromKey(key)
+			if err != nil {
+				return false
+			}
+			if a != lastAnchor {
+				select {
+				case <-ctx.Done():
+					err = ctx.Err()
+					return false
+				case ch <- a:
+					lastAnchor = a
+				}
+			}
+			return true
+		}
+		startKey := s.anchorKey(start, maxTime)
+		filter := bigtable.RowKeyFilter("^a:") // anchors only
+		err = s.t.ReadRows(ctx, bigtable.InfiniteRange(startKey), rowFn, bigtable.RowFilter(filter))
+	}()
+
+	return ch, func() error { return err }, nil
+}
+
+func (s *Store) ListAnchorRefs(ctx context.Context, a bs.Anchor) (<-chan bs.TimeRef, func() error, error) {
+	var (
+		timeRefs []bs.TimeRef
+		innerErr error
+	)
+	err := s.eachAnchorRow(ctx, a, func(row bigtable.Row) bool {
+		key := row.Key()
+		_, atime, err := s.anchorTimeFromKey(key)
+		if err != nil {
+			innerErr = errors.Wrapf(err, "parsing anchor/time from key %s", key)
+			return false
+		}
+		items := row[anchorfam]
+		if len(items) == 0 {
+			innerErr = errEmptyItems
+			return false
+		}
+		ref := bs.RefFromBytes(items[0].Value)
+		timeRefs = append(timeRefs, bs.TimeRef{T: atime, R: ref})
+		return true
+	})
+	if err != nil {
+		return nil, nil, errors.Wrap(err, "iterating over anchor rows")
+	}
+	if innerErr != nil {
+		return nil, nil, errors.Wrap(err, "processing anchor row")
+	}
+
+	ch := make(chan bs.TimeRef)
+
+	go func() {
+		defer close(ch)
+
+		for i := len(timeRefs) - 1; i >= 0; i-- {
+			select {
+			case <-ctx.Done():
+				err = ctx.Err()
+				return
+
+			case ch <- timeRefs[i]:
+				// ok
+			}
+		}
+	}()
+
+	return ch, func() error { return err }, nil
+}
+
+func (s *Store) eachAnchorRow(ctx context.Context, a bs.Anchor, f func(bigtable.Row) bool) error {
+	filter := bigtable.RowKeyFilter(fmt.Sprintf("^a:%s:\\d{20}$", regexp.QuoteMeta(string(a))))
+	return s.t.ReadRows(ctx, s.anchorRange(a), f, bigtable.RowFilter(filter))
+}
+
+func (s *Store) Put(ctx context.Context, blob bs.Blob) (bs.Ref, bool, error) {
+	mut := bigtable.NewMutation()
+	mut.Set(blobfam, blobcol, bigtable.Now(), blob)
+
+	cmut := bigtable.NewCondMutation(bigtable.LatestNFilter(1), nil, mut)
+
+	var alreadyPresent bool
+	ref := blob.Ref()
+	err := s.t.Apply(ctx, s.blobKey(ref), cmut, bigtable.GetCondMutationResult(&alreadyPresent))
+	return ref, !alreadyPresent, err
+}
+
+func (s *Store) PutMulti(ctx context.Context, blobs []bs.Blob) (bs.PutMultiResult, error) {
+	// "Conditional mutations cannot be applied in bulk and providing one will result in an error."
+	return bs.PutMulti(ctx, s, blobs)
+}
+
+func (s *Store) PutAnchor(ctx context.Context, ref bs.Ref, a bs.Anchor, when time.Time) error {
+	mut := bigtable.NewMutation()
+	mut.Set(anchorfam, anchorcol, bigtable.Time(when), ref[:])
+	return s.t.Apply(ctx, s.anchorKey(a, when), mut)
+}
+
+func (s *Store) blobKey(ref bs.Ref) string {
+	return fmt.Sprintf("b:%x", ref[:])
+}
+
+func (s *Store) refFromKey(key string) (bs.Ref, error) {
+	return bs.RefFromHex(key[2:])
+}
+
+func (s *Store) anchorKey(a bs.Anchor, when time.Time) string {
+	return fmt.Sprintf("a:%s:%020d", a, maxTime.Sub(when))
+}
+
+func (s *Store) anchorRange(a bs.Anchor) bigtable.RowSet {
+	return bigtable.PrefixRange("a:" + string(a) + ":")
+}
+
+var anchorKeyRegex = regexp.MustCompile(`^a:(.+):(\d{20})$`)
+
+func (s *Store) anchorTimeFromKey(key string) (bs.Anchor, time.Time, error) {
+	m := anchorKeyRegex.FindStringSubmatch(key)
+	if len(m) < 3 {
+		return "", time.Time{}, errors.New("malformed key")
+	}
+	nanos, err := strconv.ParseInt(m[2], 10, 64)
+	if err != nil {
+		return "", time.Time{}, errors.Wrap(err, "parsing int64")
+	}
+	return bs.Anchor(m[1]), maxTime.Add(time.Duration(-nanos)), nil
+}
+
+// This is from https://stackoverflow.com/a/32620397
+var maxTime = time.Unix(1<<63-1-int64((1969*365+1969/4-1969/100+1969/400)*24*60*60), 999999999)
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.14
 
 require (
 	cloud.google.com/go v0.60.0 // indirect
+	cloud.google.com/go/bigtable v1.5.0
 	cloud.google.com/go/storage v1.8.0
 	github.com/bobg/hashsplit v0.0.0-20200612133620-8d12abff8c50
 	github.com/cenkalti/backoff/v4 v4.0.2
@@ -12,11 +13,10 @@ require (
 	github.com/pkg/errors v0.8.0
 	github.com/rjeczalik/notify v0.9.2
 	golang.org/x/net v0.0.0-20200625001655-4c5254603344 // indirect
-	golang.org/x/sync v0.0.0-20200317015054-43a5402ce75a
+	golang.org/x/sync v0.0.0-20200625203802-6e8e738ad208
 	golang.org/x/sys v0.0.0-20200625212154-ddb9806d33ae // indirect
 	golang.org/x/text v0.3.3 // indirect
-	google.golang.org/api v0.28.0
-	google.golang.org/genproto v0.0.0-20200702021140-07506425bd67 // indirect
+	google.golang.org/api v0.29.0
 	google.golang.org/grpc v1.31.0-dev // indirect
 	google.golang.org/protobuf v1.25.0
 )
