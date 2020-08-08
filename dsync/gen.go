6111a2f191434142459318d377a27c6c19d030ae
Bob Glickstein <bobg@emphatic.com>
2020-08-08 08:32:16 -0700

    Remove dsync, in anticipation of reimplementing it in terms of fs (after some other upcoming API changes).

--- dsync/gen.go
+++
@@ -1,6 +0,0 @@
-// Package dsync contains types and functions
-// for efficiently synchronizing trees of files
-// between a primary and one or more replicas.
-//
-//go:generate protoc -I. --go_out=. dsync.proto
-package dsync
