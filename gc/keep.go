c4980c50ce0d6716c3526e8e7d5c3c1dcdcb9369
Bob Glickstein <bobg@emphatic.com>
2020-08-27 19:24:16 -0700

    Add AddAnchors.

--- gc/keep.go
+++ gc/keep.go
@@ -3,12 +3,15 @@ package gc
 import (
 	"context"
 	stderrs "errors"
+	"time"
 
 	"github.com/pkg/errors"
 	"google.golang.org/protobuf/proto"
 	"google.golang.org/protobuf/reflect/protoreflect"
+	"google.golang.org/protobuf/types/known/timestamppb"
 
 	"github.com/bobg/bs"
+	"github.com/bobg/bs/anchor"
 )
 
 // Keep is a set of refs to protect from garbage collection.
@@ -99,4 +102,87 @@ func addField(ctx context.Context, k Keep, g bs.Getter, refl protoreflect.Messag
 	}
 
 	return nil
+}
+
+// AddAnchors adds all anchors in g at or after time `since` to the Keep.
+//
+// An anchor is added if its timestamp is at or after `since`.
+// An anchor is also added if it has the latest timestamp _before_ `since` for a given name
+// (since that anchor was in effect as of time `since`),
+// unless there is another one with the same name and a timestamp exactly equal to `since`.
+//
+// The anchor blob's own ref is added,
+// and also indirectly the ref it contains,
+// plus any transitive refs that can be found.
+func AddAnchors(ctx context.Context, k Keep, g anchor.Getter, since time.Time) error {
+	// Always add the ref for an anchor's type blob.
+	_, err := k.Add(ctx, anchor.TypeRef())
+	if err != nil {
+		return err
+	}
+
+	var last *anchor.Anchor
+
+	err = g.ListAnchors(ctx, "", func(name string, ref bs.Ref, t time.Time) error {
+		// Maybe add the anchor from the previous iteration.
+		if shouldAdd(last, name, t, since) {
+			err := addAnchor(ctx, k, g, last)
+			if err != nil {
+				return err
+			}
+		}
+
+		a := &anchor.Anchor{Name: name, Ref: ref[:], At: timestamppb.New(t)}
+		last = a
+
+		if t.Before(since) {
+			return nil
+		}
+
+		return addAnchor(ctx, k, g, a)
+	})
+	if err != nil {
+		return err
+	}
+
+	if last != nil && last.At.AsTime().Before(since) {
+		return addAnchor(ctx, k, g, last)
+	}
+
+	return nil
+}
+
+func shouldAdd(a *anchor.Anchor, name string, t, since time.Time) bool {
+	if a == nil {
+		return false
+	}
+
+	atime := a.At.AsTime()
+
+	// Add it if it was the last one with its name,
+	// and it was before `since`.
+	if name != a.Name && atime.Before(since) {
+		return true
+	}
+
+	// Also add it if it has the same name as the current anchor,
+	// and was the last one before `since`
+	// (unless this one is at exactly `since`).
+	return name == a.Name && t.After(since) && atime.Before(since)
+}
+
+func addAnchor(ctx context.Context, k Keep, g bs.Getter, a *anchor.Anchor) error {
+	protoRef, err := bs.ProtoRef(a)
+	if err != nil {
+		return err
+	}
+	err = Add(ctx, k, g, protoRef)
+	if err != nil {
+		return err
+	}
+	typeRef, err := bs.TypeRef(a)
+	if err != nil {
+		return err
+	}
+	return Add(ctx, k, g, typeRef)
 }
