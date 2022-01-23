c168d0a4d978e966af7c0b5c577082df46001f1f
Bob Glickstein <bobg@emphatic.com>
2022-01-23 09:39:35 -0800

    Fix MultiErr misuse.

--- multi.go
+++ multi.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"strings"
+	"sync"
 )
 
 // GetMulti gets multiple blobs with a single call.
@@ -29,20 +30,27 @@ func GetMulti(ctx context.Context, g Getter, refs []Ref) (map[Ref]Blob, error) {
 	var (
 		res = make(map[Ref]Blob)
 		ch  = make(chan triple)
+		wg  sync.WaitGroup
 	)
 
 	for _, ref := range refs {
 		ref := ref
+		wg.Add(1)
 		go func() {
 			blob, err := g.Get(ctx, ref)
 			ch <- triple{ref: ref, blob: blob, err: err}
+			wg.Done()
 		}()
 	}
 
+	go func() {
+		wg.Wait()
+		close(ch)
+	}()
+
 	var errmap MultiErr
 
-	for i := 0; i < len(refs); i++ {
-		trip := <-ch
+	for trip := range ch {
 		if trip.err != nil {
 			if errmap == nil {
 				errmap = make(MultiErr)
@@ -53,7 +61,11 @@ func GetMulti(ctx context.Context, g Getter, refs []Ref) (map[Ref]Blob, error) {
 		res[trip.ref] = trip.blob
 	}
 
-	return res, errmap
+	// Go typed-nil pitfall: can't just return errmap if errmap is nil.
+	if errmap != nil {
+		return res, errmap
+	}
+	return res, nil
 }
 
 // MultiErr is a type of error returned by GetMulti and PutMulti.
@@ -77,7 +89,7 @@ func (e MultiErr) Error() string {
 // mapping input blobs' refs to errors encountered writing those specific blobs.
 // This function may return a successful partial result even in case of error.
 // In particular, when the error return is a MultiErr,
-// every input ref appears in either the result map or the MultiErr map.
+// the ref of every input blob appears in either the result map or the MultiErr map.
 func PutMulti(ctx context.Context, s Store, blobs []Blob) (map[Ref]bool, error) {
 	if m, ok := s.(MultiPutter); ok {
 		return m.PutMulti(ctx, blobs)
@@ -92,20 +104,27 @@ func PutMulti(ctx context.Context, s Store, blobs []Blob) (map[Ref]bool, error) 
 	var (
 		res = make(map[Ref]bool)
 		ch  = make(chan triple)
+		wg  sync.WaitGroup
 	)
 
 	for _, blob := range blobs {
 		blob := blob
+		wg.Add(1)
 		go func() {
 			ref, added, err := s.Put(ctx, blob)
 			ch <- triple{ref: ref, added: added, err: err}
+			wg.Done()
 		}()
 	}
 
+	go func() {
+		wg.Wait()
+		close(ch)
+	}()
+
 	var errmap MultiErr
 
-	for i := 0; i < len(blobs); i++ {
-		trip := <-ch
+	for trip := range ch {
 		if trip.err != nil {
 			if errmap == nil {
 				errmap = make(MultiErr)
@@ -116,5 +135,9 @@ func PutMulti(ctx context.Context, s Store, blobs []Blob) (map[Ref]bool, error) 
 		res[trip.ref] = trip.added
 	}
 
-	return res, errmap
+	// Go typed-nil pitfall: can't just return errmap if errmap is nil.
+	if errmap != nil {
+		return res, errmap
+	}
+	return res, nil
 }
